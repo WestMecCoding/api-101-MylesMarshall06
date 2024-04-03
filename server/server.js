aa89840dcaa6d38f1b4e5cf3676803d15042cc80
Myles <142362617+MylesMarshall06@users.noreply.github.com>
2024-04-03 00:23:54 +0000

    xhr stuff

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const path = require("path");
+// const cors = require("cors");
+const fs = require("fs");
 const app = express();
+// app.use(cors)
 const port = 3000;
 
-app.get("/ethan", (req, res) => {
-    res.send("hello ethan")
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "index.html"));
+
+    // res.send("hello world")
 });
 
-app.get("/", (req, res) => {
-    res.send("hello world")
+app.get("/data", (req, res) => {
+    const csvPath = path.join(__dirname, "..", "data", "zonal_sea_averages.csv");
+    
+    fs.readFile(csvPath, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        res.type("text/plain")
+        res.send(data)
+    });
 });
 
 app.listen(port, () => {
