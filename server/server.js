31f28583976c2dbe11d679b1f905f8c9a89aac6f
Myles <142362617+MylesMarshall06@users.noreply.github.com>
2024-04-04 00:14:27 +0000

    xhr stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 const fs = require("fs");
 const app = express();
-// app.use(cors)
+app.use(cors())
 const port = 3000;
 
 app.get("/", (req, res) => {
